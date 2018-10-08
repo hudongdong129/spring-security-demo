b18e8eddc92da197485f98fe6cba3a397beb79ae
hudong <924859690@qq.com>
2018-10-08 09:47:10 +0800

    提交demo修改

---
+++ src/main/java/com/hu/web/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.hu.web.controller;
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.hu.dto.User;
+
+@RestController("/user")
+public class UserController {
+	
+	@GetMapping()
+	@JsonView(User.UserSimpleView.class)
+	public List<User> query() {
+		List<User> users = new ArrayList<>();
+		users.add(new User());
+		users.add(new User());
+		users.add(new User());
+		return users;
+	}
+	
+	@GetMapping(value="/{id:\\d+ }")
+	@JsonView(User.UserDetailView.class)
+	public User getInfo(@PathVariable String id) {
+		User user = new User();
+		user.setUsername("tom");
+		return user;
+	}
+
+}
