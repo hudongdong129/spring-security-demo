00c277f369dc80980089fb4569018acada78d08b
hudong <924859690@qq.com>
2018-12-05 15:45:10 +0800

    添加新功能

---
+++ src/main/java/com/hu/wiremock/MockServer.java
@@ -0,0 +1,20 @@
+package com.hu.wiremock;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.removeAllMappings;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+public class MockServer {
+
+	public static void main(String[] args) {
+		configureFor(8000);
+		removeAllMappings();
+		stubFor(get(urlPathEqualTo("/order/1")).willReturn(aResponse().withBody("{\"id\":1}").withStatus(200)));
+	}
+
+}
